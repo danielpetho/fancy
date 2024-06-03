d3a1efecf1532521791ff07fa98e680d50e03f7f
danielpetho <kpethodaniel@gmail.com>
2024-06-03 18:59:45 +0200

    added nix flakes

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec {
+          nodejs = prev.nodejs_latest;
+          pnpm = prev.nodePackages.pnpm;
+          yarn = (prev.yarn.override { inherit nodejs; });
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ node2nix nodejs pnpm yarn python311 ] ++
+            (with python311Packages; [ pip venvShellHook ruff ]);
+        };
+      });
+    };
+}
