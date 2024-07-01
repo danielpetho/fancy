99913bd64f46982cf7ade456a2a94437d9e17387
danielpetho <kpethodaniel@gmail.com>
2024-07-01 22:01:55 +0200

    fixing eslint errors

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,4 +1,3 @@
-import { DocsSidebarNav } from "@/components/sidebar-nav"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { docsConfig } from "@/config/docs"
 
@@ -12,7 +11,6 @@ export default function Layout({
       <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10  border-l border-r border-border">
         <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
           <ScrollArea className="h-full py-6 pr-6 lg:py-8">
-            <DocsSidebarNav items={docsConfig} />
           </ScrollArea>
         </aside>
         <div className="w-full border border-border">
