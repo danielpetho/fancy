bd494a7ffd86b649f90d929c29d0b06a9ef99cae
danielpetho <kpethodaniel@gmail.com>
2024-10-24 17:18:48 +0200

    fix now?

--- src/fancy/examples/text/scroll-and-swap-text-demo.tsx
+++ src/fancy/examples/text/scroll-and-swap-text-demo.tsx
@@ -8,31 +8,32 @@ export default function Preview() {
     <div
       className="w-full h-full rounded-lg items-center justify-center font-overusedGrotesk p-2 overflow-auto overscroll-auto bg-background relative"
       ref={containerRef}
-      data-lenis-prevent
     >
-      <div className="h-[100%] flex justify-center items-center uppercase">
+      <div className="h-[100%] flex justify-center items-center uppercase relative">
+        <p className="absolute bottom-4 left-4 font-bold text-xl">
+          SCROLL SLOWLY
+        </p>
         <div className="flex text-6xl justify-center items-center flex-col text-black">
           <ScrollAndSwapText
             label="Every day is a journey,"
-            offset={["0 0.8", "0 1"]}
+            offset={["0 0.2", "0 0.35"]}
             className="font-bold "
             containerRef={containerRef}
           />
           <ScrollAndSwapText
             label="and the journey"
-            offset={["0 0.9", "0 1.15"]}
+            offset={["0 0.25", "0 0.45"]}
             className="font-bold "
             containerRef={containerRef}
           />
           <ScrollAndSwapText
             label=" itself is home."
-            offset={["0 1", "0 1.25"]}
+            offset={["0 0.3", "0 0.5"]}
             className="font-bold"
             containerRef={containerRef}
           />
         </div>
       </div>
-      <p className="absolute bottom-4 left-4 font-bold text-xl">SCROLL SLOWLY</p>
       <div className="h-[30%]"></div>
     </div>
   );
