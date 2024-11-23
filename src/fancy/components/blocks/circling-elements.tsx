853d5da66c6b1ae1e0040b678dcec37010d56417
danielpetho <kpethodaniel@gmail.com>
2024-11-23 18:08:22 +0100

    extend circling element doc

--- src/fancy/components/blocks/circling-elements.tsx
+++ src/fancy/components/blocks/circling-elements.tsx
@@ -1,5 +1,5 @@
-import { motion, useAnimationFrame } from "framer-motion";
-import React, { useState } from "react";
+import { motion } from "framer-motion";
+import React from "react";
 
 type CirclingElementsProps = {
   children: React.ReactNode;
