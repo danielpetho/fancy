d710b49f2f29f7a18ace82f28cff56b87d9f0faa
danielpetho <kpethodaniel@gmail.com>
2024-12-15 11:51:19 +0100

    remove redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { LandingHero } from "@/components/landing/landing-hero";
 import { getAllComponents } from "@/lib/api";
-import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  
-  //redirect('/docs/introduction');
-  
+    
   const allComps = await getAllComponents(true);
 
   return (
