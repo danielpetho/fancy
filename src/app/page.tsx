3881419b73beb4ad39eb28a8b373f5a2fc0627e7
danielpetho <kpethodaniel@gmail.com>
2025-01-17 19:51:21 +0100

    fix: show landing even without content from contentful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,11 @@ export default async function Home() {
   try {
     const allComps = await getAllComponents(true)
 
-    if (!allComps) {
-      throw new Error("Failed to fetch components")
-    }
 
     return <LandingHero allComps={allComps} />
   } catch (error) {
     console.error("Error fetching components:", error)
     // You might want to return an error state here
-    return <div>Error loading components</div>
+    return <LandingHero allComps={null} />
   }
 }
