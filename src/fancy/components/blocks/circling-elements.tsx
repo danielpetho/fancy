f7e7b016774d58f99a7b5a006a2c651537357e0e
danielpetho <kpethodaniel@gmail.com>
2025-03-04 18:00:37 +0100

    fix: fix anim, now?

--- src/fancy/components/blocks/circling-elements.tsx
+++ src/fancy/components/blocks/circling-elements.tsx
@@ -2,6 +2,7 @@
 
 import React, { useMemo } from "react"
 import { motion } from "motion/react"
+
 import { cn } from "@/lib/utils"
 
 type CirclingElementsProps = {
@@ -23,18 +24,21 @@ const CirclingElements: React.FC<CirclingElementsProps> = ({
     <div className={`relative w-full h-full ${className}`}>
       {React.Children.map(children, (child, index) => {
         const offset = (index * 360) / React.Children.count(children)
+
+        const animationProps = {
+          "--circling-duration": duration,
+          "--circling-radius": radius,
+          "--circling-offset": offset,
+          animation: `circling ${duration}s ${easing} infinite`,
+        } as React.CSSProperties
+
         return (
           <motion.div
             key={index}
-            className={cn("transform-gpu absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2")}
-            style={
-              {
-                "--circling-duration": duration,
-                "--circling-radius": radius,
-                "--circling-offset": offset,
-                animation: `circling ${duration}s infinite ${easing}`,
-              } as React.CSSProperties
-            }
+            className={cn(
+              "transform-gpu absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-circling"
+            )}
+            style={animationProps}
           >
             {child}
           </motion.div>
