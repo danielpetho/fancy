6387ad742b51fdd4f6973b30fc3e64262cc9b244
Khoa Phan <pldkhoa96.work@gmail.com>
2025-04-19 13:23:44 +0700

    feat: finish - add more demo, usage, understanding the component

--- src/fancy/examples/image/image-trail-demo.tsx
+++ src/fancy/examples/image/image-trail-demo.tsx
@@ -9,7 +9,14 @@ import {
 const ImageTrailDemo = () => {
   return (
     <div className="w-full h-full bg-white relative text-foreground dark:text-muted">
-      <ImageTrail threshold={80}>
+      <ImageTrail
+        threshold={80}
+        keyframes={{ opacity: [0, 1, 1, 0], scale: [1, 1, 2] }}
+        keyframesOptions={{
+          opacity: { duration: 2, times: [0, 0.001, 0.9, 1] },
+          scale: { duration: 2, times: [0, 0.8, 1] },
+        }}
+      >
         {exampleImages.map((image, index) => (
           <ImageTrailItem key={index}>
             <div className="w-28 h-24 relative overflow-hidden">
