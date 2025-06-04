8a595e9420123e6426d7ecf2d5404ef108f984cc
danielpetho <kpethodaniel@gmail.com>
2025-06-04 10:09:02 +0200

    feat: no need for async page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,7 @@ import { getAllComponents } from "@/lib/get-components"
 import { LandingHero } from "@/components/landing/landing-hero"
 
 export default function Home() {
-  try {
-    const allComps = getAllComponents()
+  const allComps = getAllComponents()
 
-    return <LandingHero allComps={allComps} />
-  } catch (error) {
-    console.error("Error fetching components:", error)
-    // You might want to return an error state here
-    return <LandingHero allComps={null} />
-  }
+  return <LandingHero allComps={allComps} />
 }
