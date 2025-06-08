a1af3b5bd0a7d466c2384740949cfc990db26ef8
danielpetho <kpethodaniel@gmail.com>
2025-06-08 20:06:50 +0200

    fix: fix components page

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -9,8 +9,8 @@ export default async function ComponentsPage() {
   const components = getAllComponents()
 
   return (
-    <main className="w-screen">
-      <div className="rounded-xl bg-background py-6 lg:gap-10 lg:py-6 border-border border shadow-lg px-6 ">
+    <main className="flex-1 justify-center w-full">
+      <div className="rounded-2xl bg-background py-6 lg:gap-10 lg:py-6 border-border border p-6">
         <h1 className="text-4xl font-bold mb-8 font-calendas">Components</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
           {components.map((component) => (
