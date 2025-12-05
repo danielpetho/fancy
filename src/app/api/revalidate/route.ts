10e9556d9bbacf089087f691faf399e5fb8668fc
Daniel Petho <kpethodaniel@gmail.com>
2025-12-05 11:12:30 +0100

    feat: update revalidate tag

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 })
   }
 
-  revalidateTag("components")
+  revalidateTag("components", "max")
 
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
